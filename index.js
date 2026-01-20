bd2d1d16a4684d7ea46d32579cce63384e7b15d2
LluisArjuna <lluisarjunav@gmail.com>
2026-01-20 12:16:32 +0100

    we pickup all the elements we need from the html. The button we'll use to display the function and the number introduced by the user.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import { lottery } from "./src/scripts/lotteryLogic.js";
 import { showResult } from "./src/scripts/lotteryUI.js";
+
+const button = document.getElementById('button');
+const input = document.getElementById('userNumber');
+
