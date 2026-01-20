8ec198db30407f5fae51b8d28288bc6f44f18eb6
LluisArjuna <lluisarjunav@gmail.com>
2026-01-20 12:09:42 +0100

    We've piched the element were we want to introduce the results and we've checked if the response message is "Win" or "Lose". If that's the case we show the number entered, the number of the machine and the outcome of the game. If we didn't introduce a number or it's out of range an alert will apear showing the error.

--- src/scripts/lotteryUI.js
+++ src/scripts/lotteryUI.js
@@ -1,3 +1,10 @@
 export const showResult = (response) => {
-
+    const results = document.getElementById("results");
+    if (response.message === 'Win' || response.message === 'Lose') {
+        const result = document.createElement('p')
+        result.innerHTML = `Ingresado: ${response.data.userNumber} | Aleatorio: ${response.data.lotteryInput} | Resultado: ${response.data.message}`
+        results.appendChild(result)
+    } else {
+        alert(response.value)
+    }
 }
