b70dd5ddfe63ae1acfa323e0b84d625e3584cdb9
LluisArjuna <lluisarjunav@gmail.com>
2026-01-20 11:44:11 +0100

    We've defined the return of the function. If there's no error we'll return an object with the message "Win" or "Lose" and the nombers given by the user and the computer. If there's an error we'll show the error message

--- src/scripts/lotteryLogic.js
+++ src/scripts/lotteryLogic.js
@@ -1,17 +1,24 @@
-export const playLottery = (userNum, lotteryNum) =>{
+export const playLottery = (userInput, lotteryNum) =>{
     try {
-        if (typeof(userNum) !== 'number'){
+        if (typeof(userInput) !== 'number'){
             throw new Error("The value introduced is not a number!");
             
-        } else if (userNum < 1 || userNum > 10) {
+        } else if (userInput < 1 || userInput > 10) {
             throw new Error("You must introduce a number between 1 and 10");
         }
 
-        const answer = userNum === lotteryNum ? 'Win' : 'Lose';
+        const answer = userInput === lotteryNum ? 'Win' : 'Lose';
 
+        return {
+            message: answer, 
+            data: {
+                userNumber: userInput, 
+  		        randomNumber: lotteryNum 
+            }
+        }
         
     } catch (error) {
-        
+        return error.message
     }
     
 }
