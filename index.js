24e6c7681a040cb0c1ff750477ce54c34779e64b
LluisArjuna <lluisarjunav@gmail.com>
2026-01-20 12:21:58 +0100

    we define the addEventListener atached to the button: First we transform the nomber given by the user to a type Number. then we execute the function lottery given the user number and show the results.

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ import { showResult } from "./src/scripts/lotteryUI.js";
 const button = document.getElementById('button');
 const input = document.getElementById('userNumber');
 
+button.addEventListener('click', () =>{
+    const userNumber = Number(input.value);
+    const result = lottery(userNumber);
+    showResult(result)
+})
