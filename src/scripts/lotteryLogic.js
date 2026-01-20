df2aba598737326983990c430c78f716ed954181
LluisArjuna <lluisarjunav@gmail.com>
2026-01-20 11:39:04 +0100

    Now we've created an element that checks if the number of the user and the computer number are the same. If they are the same, the value will be "Win", if not the value will be "Lose"

--- src/scripts/lotteryLogic.js
+++ src/scripts/lotteryLogic.js
@@ -1,8 +1,17 @@
 export const playLottery = (userNum, lotteryNum) =>{
-    if (typeof(userNum) !== 'number'){
-        throw new Error("The value introduced is not a number!");
+    try {
+        if (typeof(userNum) !== 'number'){
+            throw new Error("The value introduced is not a number!");
+            
+        } else if (userNum < 1 || userNum > 10) {
+            throw new Error("You must introduce a number between 1 and 10");
+        }
+
+        const answer = userNum === lotteryNum ? 'Win' : 'Lose';
+
         
-    } else if (userNum < 1 || userNum > 10) {
-        throw new Error("You must introduce a number between 1 and 10");
+    } catch (error) {
+        
     }
+    
 }
