5f414cf2fb2bf0d89f024cb64636bb34e4123b1f
LluisArjuna <lluisarjunav@gmail.com>
2026-01-20 12:25:34 +0100

    Changes in the innerHTML at line 5 because the object atributes had a diferent name. At line 8 there was no need to specify response.value so we specified only response.

--- src/scripts/lotteryUI.js
+++ src/scripts/lotteryUI.js
@@ -2,9 +2,9 @@ export const showResult = (response) => {
     const results = document.getElementById("results");
     if (response.message === 'Win' || response.message === 'Lose') {
         const result = document.createElement('p')
-        result.innerHTML = `Ingresado: ${response.data.userNumber} | Aleatorio: ${response.data.lotteryInput} | Resultado: ${response.data.message}`
+        result.innerHTML = `Ingresado: ${response.data.userNumber} | Aleatorio: ${response.data.randomNumber} | Resultado: ${response.message}`
         results.appendChild(result)
     } else {
-        alert(response.value)
+        alert(response)
     }
 }
