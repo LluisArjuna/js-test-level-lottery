6d094278f6a9de0546ebe0852d7d2e89a7c5cb03
LluisArjuna <lluisarjunav@gmail.com>
2026-01-20 11:33:49 +0100

    Defining the function playLottery: we're checking that the value introduced by the user is a number and that it's range is between 1 and 10.

--- src/scripts/lotteryLogic.js
+++ src/scripts/lotteryLogic.js
@@ -1,3 +1,8 @@
 export const playLottery = (userNum, lotteryNum) =>{
-
+    if (typeof(userNum) !== 'number'){
+        throw new Error("The value introduced is not a number!");
+        
+    } else if (userNum < 1 || userNum > 10) {
+        throw new Error("You must introduce a number between 1 and 10");
+    }
 }
